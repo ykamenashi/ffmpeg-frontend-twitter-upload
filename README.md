13d1980fd3f12ac953dba1e5092d380eaf077ecc
ykamenashi <kamenashi@cloud-ace.jp>
2022-05-08 07:24:30 +0900

    1st

--- README.md
+++ README.md
@@ -1,2 +1,28 @@
 # ffmpeg-frontend-twitter-upload
-Enable uploading mp3 to twitter without movie(by fixed picture)
+
+rename of `fixedImageVideoConvert`
+output video file for Twitter / YouTube with ffmpeg. ( depend on ffmpeg )
+
+## Correctly working checked ffmpeg version with:
+* ffmpeg version >= 4.3.1
+
+## Limitations
+
+* Twitter only accepts the video shorter than 1:30.
+
+## Syntax
+
+```bash
+./vidconv.sh OUTPUT_FILE_PATH INPUT_PICT_PATH INPUT_MP3_PATH
+```
+
+* OUTPUT_FILE_PATH
+** .mp4 filename. does not overwrite.
+* INPUT_PICT_PATH
+** .jpg, .png, .webp, and so on
+* INPUT_MP3_PATH
+** .mp3, .aac, .ogg, and so on
+
+## If you use MacOS
+
+* This command notifies you ending process by voice.
---
+++ vidconv.sh
@@ -0,0 +1,62 @@
+#!/usr/bin/env bash
+
+### usage
+function usage {
+  cat << HELP
+  usage : $0 : simple ffmpeg frontend for musicians, with fixed picture.
+
+  \$0 <output filename> <input Picture filename> <input MP3 filename>
+
+  -h : show this help.
+  \$1 : output Path with filename.
+  \$2 : input Picture Path with filename.
+  \$3 : input MP3 Path with filename.
+
+  **Please rename already processed video file, before execute this script.**
+HELP
+  exit 254
+}
+
+function errq {
+  echo $1
+  exit $2
+}
+
+### show help
+if [[ "$1" =~ [-]+[h]* ]] ; then usage; fi
+
+### Number of arguments are must 3
+if [[ $# -ne 3 ]] ; then usage; fi
+
+### ffmpeg existing check
+command -v ffmpeg > /dev/null 2>&1
+readonly EXIST_CHECK_CODE=$?
+if [ $EXIST_CHECK_CODE -ne 0 ] ; then usage ; fi
+
+### input pict file existing check
+readonly INPUT_PICT_FILE="$2"
+if [ ! -f "$INPUT_PICT_FILE" ] ; then errq "Input file not found. quit." 253 ; fi
+
+### input MP3 file existing check
+readonly INPUT_MP3_FILE="$3"
+if [ ! -f "$INPUT_MP3_FILE" ] ; then errq "Input file not found. quit." 252 ; fi
+
+### output file not existing check
+readonly OUTPUT_FILE="$1"
+if [ -f "$OUTPUT_FILE" ] ; then errq "Output file already exists. quit." 251; fi
+
+### convert process
+readonly CMDLINEOPT="-shortest"
+ffmpeg -loop 1 -i "$INPUT_PICT_FILE" -i "$INPUT_MP3_FILE" "$CMDLINEOPT" -acodec aac -pix_fmt yuv420p "$OUTPUT_FILE" 
+readonly RETVAL=$?
+
+### notify to user ( use bell? )
+if [ "$(uname)" == "Darwin" ] ; then ## is Mac
+  if [ "$RETVAL" -eq 0 ]; then
+    say Convert is done.
+  fi
+fi
+
+### end section
+
+exit $RETVAL
